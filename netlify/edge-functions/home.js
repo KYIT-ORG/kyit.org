74f1176748eab8a9995716a4a3be1a2f3acbbb26
KYIT-ORG <91833093+KYIT-ORG@users.noreply.github.com>
2022-10-03 17:04:57 +0800

    Update home.js

--- netlify/edge-functions/home.js
+++ netlify/edge-functions/home.js
@@ -1,11 +1,43 @@
 export default async(r, c) => {
   
+  let abc = `
+  <!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>{{ title }}</title>
+    
+    
+
+</head>
+
+<body style="background: #333; color: #000;">
+
+    <header>
+        <a href="/">Know Your IT</a>
+    </header>
+    
+   
+    <main>
+
+        <h1>At edge...</h1>
+
+
+    </main>
+
+    <footer> &copy; 2021 KYIT.org</footer>
+</body>
+
+</html>
+  `
+  
 //     let ua = r.headers.get('user-agent')
 
 //     let is_curl = ua.includes('curl')
 
 //     console.log(r.headers, '\n\n@@@\n\n', c, '\n\n@@@\n\n')
 
-    return new Response('Welcome to KYIT.org!!!')
+    return new Response(abc)
 
 }
