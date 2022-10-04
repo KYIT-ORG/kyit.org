0fb1b52e15c8f24cec3de21ef969bb7ebbac542e
KYIT-ORG <91833093+KYIT-ORG@users.noreply.github.com>
2022-10-04 11:51:55 +0800

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -15,7 +15,7 @@ export default async(r, c) => {
   
     //let t = '\033[32m This is red text \033[0m'
     //let t = 'hello 333'
-    let t = '\x1b[31msfsf\x1b[0m'
+    let t = '\x1b[31maaaaaa\n\x1b[60mbbbbbb\n\n'
   
     return new Response(t)
 
