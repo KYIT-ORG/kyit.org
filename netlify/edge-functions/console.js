3475274bf45d74f58e3f4c253e0a91f252e01ba5
KYIT-ORG <91833093+KYIT-ORG@users.noreply.github.com>
2022-10-04 16:27:08 +0800

    Update console.js

--- netlify/edge-functions/console.js
+++ netlify/edge-functions/console.js
@@ -10,6 +10,7 @@ export default async(r, c) => {
     let t1 = `\x1b[1;33m\n
     Your IP: ${c.ip} \n
     Your Location: ${c.geo.city} - ${c.geo.subdivision.name} - ${c.geo.country.name} \n
+    Your Browser: ${r.headers.get('user-agent')} \n
     \x1b[0m`
     
     return new Response(t1)
