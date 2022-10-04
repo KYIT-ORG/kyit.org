9d673edec88aa758d171e3c8adb88b55e53d3c38
KYIT-ORG <91833093+KYIT-ORG@users.noreply.github.com>
2022-10-04 16:51:44 +0800

    Update console.js

--- netlify/edge-functions/console.js
+++ netlify/edge-functions/console.js
@@ -1,10 +1,10 @@
 export default async(r, c) => {
   
-    let ua = r.headers.get('user-agent')
+    //let ua = r.headers.get('user-agent')
 
-    let is_curl = ua.includes('curl')
+    //let is_curl = ua.includes('curl')
    
-    if(!is_curl) return
+    //if(!is_curl) return
   
 
     let t1 = `\x1b[1;33m\n
